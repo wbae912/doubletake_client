1804eb2ddfa8096bf722c2bc71a42de30dba891f
William Bae <wbae912@gmail.com>
2020-01-31 15:23:28 -0800

    Created General Service object that handles all API requests for /api/general endpoint

---
+++ src/Utils/general-service.js
@@ -0,0 +1,81 @@
+import TokenService from '../services/token-service';
+
+const GeneralService = {
+  getLists() {
+    return fetch('http://localhost:8000/api/general', {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if(!res.ok) {
+          return res.json().then(err => Promise.reject(err));
+        }
+        return res.json();
+      })
+  },
+
+  postList(newList) {
+    return fetch('http://localhost:8000/api/general', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(newList)
+    })
+    .then(res => {
+      if(!res.ok) {
+        return res.json().then(err => Promise.reject(err));
+      }
+      return res.json();
+    })
+  },
+
+  getSpecificList(id) {
+    return fetch(`http://localhost:8000/api/general/${id}`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if(!res.ok) {
+          return res.json().then(err => Promise.reject(err));
+        }
+        return res.json();
+      })
+  },
+
+  deleteList(id) {
+    return fetch(`http://localhost:8000/api/general/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    .then(res => {
+      if(!res.ok) {
+        return res.json().then(err => Promise.reject(err));
+      }
+    })
+  },
+
+  editList(id, editList) {
+    return fetch(`http://localhost:8000/api/general/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(editList)
+    })
+    .then(res => {
+      if(!res.ok) {
+        return res.json().then(err => Promise.reject(err));
+      }
+    })
+  }
+}
+
+export default GeneralService;
