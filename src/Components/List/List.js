0503c13f60719cdde3ff4c543203ad0e61d002fc
William Bae <wbae912@gmail.com>
2020-03-04 16:56:42 -0800

    DELETE method for General Lists done

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -6,77 +6,124 @@ import Items from '../Items/Items';
 import './List.css';
 
 export default class List extends Component {
-  // static contextType = ListContext;
+  static contextType = ListContext;
   
-  // constructor(props) {
-  //   super(props)
+  constructor(props) {
+    super(props)
   
-  //   this.state = {
-  //      deleteClicked: false,
-  //      editClicked: false,
-  //      lists: []
-  //   }
-  // }
+    this.state = {
+       deleteClicked: false,
+       editClicked: false,
+       lists: []
+    }
+  }
   
-  // toggleButton = e => {
-  //   this.setState({
-  //     [e.target.name]: true
-  //   })
-  // }
+  // DELETE methods
+  toggleButton = e => {
+    this.setState({
+      [e.target.name]: true
+    })
+  }
   
-  // yesClicked = () => {
-  //   GeneralService.deleteList(this.props.list.id)
-  //     .then(() => {
-  //       this.setState({
-  //         deleteClicked: false
-  //       })
+  yesClicked = () => {
+    GeneralService.deleteList(this.props.list.id)
+      .then(() => {
+        this.setState({
+          deleteClicked: false
+        })
 
-  //       /* The steps below trigger the page to re-render when a list is successfully deleted. Otherwise, you would need to refresh the page to see that the list was 
-  //          deleted, which is not what is intended.
+        /* The steps below trigger the page to re-render when a list is successfully deleted. Otherwise, you would need to refresh the page to see that the list was 
+           deleted, which is not what is intended.
 
-  //          ADDITIONAL NOTES: setState for deleteClicked and yesClicked have to come BEFORE "resetting" the generalLists in context. Otherwise, a warning is received
-  //          that setState is being called on an unmounted component (which can lead to performance issues). If we reset the generalLists with the removed list and call setState 
-  //          afterwards, the component has already been deleted/unmounted, which is why setState needs to come before
-  //       */
-  //       const filteredGeneralList = this.context.generalLists.filter(list => list.id !== this.props.list.id);
-  //       this.context.setGeneralLists(filteredGeneralList);
-  //     })
-  //     .catch(res => {
-  //       this.context.setError(res.error);
-  //     })
-  // }
+           ADDITIONAL NOTES: setState for deleteClicked and yesClicked have to come BEFORE "resetting" the generalLists in context. Otherwise, a warning is received
+           that setState is being called on an unmounted component (which can lead to performance issues). If we reset the generalLists with the removed list and call setState 
+           afterwards, the component has already been deleted/unmounted, which is why setState needs to come before
+        */
+        const generalLists = [...this.context.generalLists];
+        const filteredGeneralList = generalLists.filter(list => list.id !== this.props.list.id);
+        this.context.setGeneralLists(filteredGeneralList);
+      })
+      .catch(res => {
+        this.context.setError(res.error);
+      })
+  }
 
-  // noClicked = () => {
-  //   this.setState({
-  //     deleteClicked: false
-  //   })
-  // }
+  noClicked = () => {
+    this.setState({
+      deleteClicked: false
+    })
+  }
   
-  // renderDeleteConfirmMessage = () => {
-  //   if(this.state.deleteClicked) {
-  //     return (
-  //       <div className="delete-confirm">
-  //         <h3 className="delete-h3">Are you sure you want to delete this list?</h3>
-  //         <div className="delete-confirm-buttons">
-  //           <button 
-  //             type="button" 
-  //             className="yes-button"
-  //             onClick={this.yesClicked}
-  //           >
-  //           Yes</button>
-  //           <button 
-  //             type="button" 
-  //             className="no-button"
-  //             onClick={this.noClicked}
-  //           >
-  //           No</button>
-  //         </div>
-  //       </div>
-  //     )
-  //   }
-  // }
+  renderDeleteConfirmMessage = () => {
+    if(this.state.deleteClicked) {
+      return (
+        <div className="delete-confirm">
+          <h3 className="delete-h3">Are you sure you want to delete this list?</h3>
+          <div className="delete-confirm-buttons">
+            <button 
+              type="button" 
+              className="yes-button"
+              onClick={this.yesClicked}
+            >
+            Yes</button>
+            <button 
+              type="button" 
+              className="no-button"
+              onClick={this.noClicked}
+            >
+            No</button>
+          </div>
+        </div>
+      )
+    }
+  }
 
-  // handleCancel = e => {
+  // PATCH methods
+
+  render() {
+    return (
+      <div className="list-entry">
+        <h2 className="list-h2">{this.props.list.title}</h2>
+
+        <Items 
+          userId={this.props.list.user_id}
+          listId={this.props.list.id}
+        />
+
+        <div className="list-buttons">
+          <button
+            type="button"
+            className="delete-button"
+            name="deleteClicked"
+            onClick={this.toggleButton}
+          >
+          Delete</button>
+          <button
+            type="button"
+            className="edit-button"
+            name="editClicked"
+          >
+          Edit</button>
+        </div>
+
+        {this.renderDeleteConfirmMessage()}
+      </div>
+    )
+  }
+}
+
+
+// NOTES: REFACTOR CODE TO NOT MUTATE STATE...
+// NOTES: DO NOT USE INDEX AS KEY OR ELSE IT WILL CHANGE THE ORDER OF THINGS....FIX THAT IN LIST.JS
+
+// FIX EDIT BUG
+// ALSO...LOOK INTO PASSING PROPS AS AN ID... WILL IT ADD IT AS A PROPERTY??? ID = 0; key={id++}, id={id++}
+
+
+
+
+
+ // handleCancel = e => {
   //   this.setState({
   //     editClicked: false
   //   })
@@ -183,39 +230,3 @@ export default class List extends Component {
   //     </div>
   //   )
   // }
-
-  render() {
-    return (
-      <div className="list-entry">
-        <h2 className="list-h2">{this.props.list.title}</h2>
-
-        <Items 
-          userId={this.props.list.user_id}
-          listId={this.props.list.id}
-        />
-
-        <div className="list-buttons">
-          <button
-            type="button"
-            className="delete-button"
-            name="deleteClicked"
-          >
-          Delete</button>
-          <button
-            type="button"
-            className="edit-button"
-            name="editClicked"
-          >
-          Edit</button>
-        </div>
-      </div>
-    )
-  }
-}
-
-
-// NOTES: REFACTOR CODE TO NOT MUTATE STATE...
-// NOTES: DO NOT USE INDEX AS KEY OR ELSE IT WILL CHANGE THE ORDER OF THINGS....FIX THAT IN LIST.JS
-
-// FIX EDIT BUG
-// ALSO...LOOK INTO PASSING PROPS AS AN ID... WILL IT ADD IT AS A PROPERTY??? ID = 0; key={id++}, id={id++}
