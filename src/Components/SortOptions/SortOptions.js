27713a674314b544e3570911763e334f8ce5acc4
William Bae <wbae912@gmail.com>
2020-04-13 16:05:18 -0700

    SortOptions component styled

--- src/Components/SortOptions/SortOptions.js
+++ src/Components/SortOptions/SortOptions.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import ListContext from '../../Context/ListContext';
+import './SortOptions.css';
 
 class SortOptions extends Component {
   static contextType = ListContext;
